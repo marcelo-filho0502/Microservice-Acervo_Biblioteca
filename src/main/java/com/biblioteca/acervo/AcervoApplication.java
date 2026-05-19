2ba524a53e62dd0fb01d2d454f958be7dd3627fb
XGUDDDI <marcelomrfilho@gmail.com>
2026-05-19 16:59:28 -0300

    Subindo Spring

---
+++ src/main/java/com/biblioteca/acervo/AcervoApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.acervo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/acervo/AcervoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.acervo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
